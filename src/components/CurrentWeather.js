5579f87b17984e31471adc5a6a117473c276fa60
fawwaz95 <fawwaz_95@hotmail.com>
2023-05-13 11:46:54 -0400

    src/components/CurrentWeather.js

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -10,13 +10,13 @@ const CurrentWeather = (props) => {
         <div className='m-10 p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 font-bold text-white font-mono border-solid border-2 border-white-600 drop-shadow-xl rounded-3xl'>
             <p className='text-3xl sm:text-4xl md:text-5xl lg:md:text-6xl font-bold order-first sm:order-first md:order-first lg:order-first'>{city}</p>
             <ShowWeatherIcon className='order-2 sm:order-3 md:order-3 lg:order-3' weatherDesc={weatherDesc} isNight={props.isNight} />
-            <p className='order-3 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-10'>{weatherDesc}</p>
+            <p className='order-3 text-base font-bold sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-10'>{weatherDesc}</p>
             <p className='order-4 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-11'>Temperature {temp}°C</p>
-            <p className='order-5 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-13'>Feels like {feelsLike}°C {props.isNight}</p>
+            <p className='order-5 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-13'>Feels Like {feelsLike}°C {props.isNight}</p>
             <p className='order-6 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-2'>Windspeed {wind} km/h</p>
             <p className='order-7 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-4'>Visiblity {visibility} km</p>
             <p className='order-8 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-6'>Humidity {humidity}%</p>
-            <p className='order-9 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-9'>Current time {currentTime}</p>
+            <p className='order-9 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-9'>Current Time {currentTime}</p>
             <p className='order-10 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm-order-12'>Sunrise {sunrise}</p>
             <p className='order-11 text-base sm:text-lg lg:text-2xl 2xl:text-3xl sm:order-14'>Sunset {sunset}</p>      
         </div>
