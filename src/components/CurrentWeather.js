33e7b0f24f152bb2b67e922445000e7f151a8c8b
fawwaz95 <fawwaz_95@hotmail.com>
2023-05-13 11:34:08 -0400

    testing resp design

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -8,17 +8,17 @@ const CurrentWeather = (props) => {
     return (      
         //If on mobile grid will follow grid-cols-1. If small screen it will use sm. If medium screen it will use md. If large screen it will use lg.
         <div className='m-10 p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 font-bold text-white text-2xl font-mono border-solid border-2 border-white-600 drop-shadow-xl rounded-3xl'>
-            <p className='text-3xl font-bold order-first sm:order-first md:order-first lg:order-first'>{city}</p>
+            <p className='text-3xl sm:text-4xl md:text-5xl lg:md:text-6xl font-bold order-first sm:order-first md:order-first lg:order-first'>{city}</p>
             <ShowWeatherIcon className='order-2 sm:order-3 md:order-3 lg:order-3' weatherDesc={weatherDesc} isNight={props.isNight} />
-            <p className='order-3 sm:order-10'>{weatherDesc}</p>
-            <p className='order-4 text-md lg:text-2xl 2xl:text-3xl sm:order-11'>Temperature {temp}°C</p>
-            <p className='order-5 text-md lg:text-2xl 2xl:text-3xl sm:order-13'>Feels like {feelsLike}°C {props.isNight}</p>
-            <p className='order-6 text-md lg:text-2xl 2xl:text-3xl sm:order-2'>Windspeed {wind} km/h</p>
-            <p className='order-7 text-md lg:text-2xl 2xl:text-3xl sm:order-4'>Visiblity {visibility} km</p>
-            <p className='order-8 text-md lg:text-2xl 2xl:text-3xl sm:order-6'>Humidity {humidity}%</p>
-            <p className='order-9 text-md lg:text-2xl 2xl:text-3xl sm:order-9'>Current time {currentTime}</p>
-            <p className='order-10 text-md lg:text-2xl 2xl:text-3xl sm-order-12'>Sunrise {sunrise}</p>
-            <p className='order-11 text-md lg:text-2xl 2xl:text-3xl sm:order-14'>Sunset {sunset}</p>      
+            <p className='order-3 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-10'>{weatherDesc}</p>
+            <p className='order-4 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-11'>Temperature {temp}°C</p>
+            <p className='order-5 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-13'>Feels like {feelsLike}°C {props.isNight}</p>
+            <p className='order-6 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-2'>Windspeed {wind} km/h</p>
+            <p className='order-7 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-4'>Visiblity {visibility} km</p>
+            <p className='order-8 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-6'>Humidity {humidity}%</p>
+            <p className='order-9 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-9'>Current time {currentTime}</p>
+            <p className='order-10 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm-order-12'>Sunrise {sunrise}</p>
+            <p className='order-11 text-sm sm:text-md lg:text-2xl 2xl:text-3xl sm:order-14'>Sunset {sunset}</p>      
         </div>
     )
 }
