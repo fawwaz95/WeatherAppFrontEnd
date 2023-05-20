dee57e22831ba9518b7b477e154bc1ab08663ac8
fawwaz95 <fawwaz_95@hotmail.com>
2023-05-20 19:49:53 -0400

    changed bg

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -152,7 +152,7 @@ class SearchField extends React.Component {
                     <div className="relative w-3/4">
                       <input ref={this.inputRef}  
                              onChange={this.onChangeSearchField} 
-                             className="block p-2.5 w-full z-20 text-sm rounded-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
+                             className="block p-2.5 w-full z-20 text-sm rounded-lg border-l-gray-50 border-l-2 border border-gray-300" 
                              placeholder="Search a City..." 
                              required/>
                       <button type="submit" 
